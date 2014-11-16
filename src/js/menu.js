73918b480e9f028b7884eec9658376788f07b6fc
cravesoft <cravesoft@gmail.com>
2014-11-16 23:55:37 +0100

    Change text style for instructions

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,9 +11,14 @@
         create: function () {
             this.background = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'background');
 
-            var text = 'Dodger\n\nClick to start'
+            var text = 'Dodger'
               , style = { font: '40px Arial', fill: '#ffffff', align: 'center' }
               , t = this.add.text(this.game.world.centerX, this.game.world.centerY, text, style);
+            t.anchor.set(0.5, 0.5);
+
+            text = 'Click to start'
+            style = { font: '30px Arial', fill: '#ffffff', align: 'center' }
+            t = this.add.text(this.game.world.centerX, this.game.world.centerY + 80, text, style);
             t.anchor.set(0.5, 0.5);
 
             this.input.onDown.add(this.onDown, this);
