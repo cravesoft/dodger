94dceb3f076b8be57a967324b806cb6bca4646b9
cravesoft <cravesoft@gmail.com>
2014-11-22 22:55:36 +0100

    Fixed scaling issues

--- src/js/boot.js
+++ src/js/boot.js
@@ -5,23 +5,40 @@
 
     Boot.prototype = {
         
+        init: function () {
+            // Unless you specifically know your game needs to support
+            // multi-touch I would recommend setting this to 1
+            this.input.maxPointers = 1;
+
+            // Phaser will automatically pause if the browser tab the game is
+            // in loses focus. You can disable that here:
+            this.stage.disableVisibilityChange = true;
+
+            // Loading screen will have a black background
+            this.stage.backgroundColor = '#000';
+
+            if(this.game.device.desktop) {
+                this.scale.pageAlignHorizontally = true;
+            } else {
+                this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                this.scale.setMinMax(480, 260, 1280, 720);
+
+                // Have the game centered
+                this.scale.pageAlignHorizontally = true;
+                this.scale.pageAlignVertically = true;
+
+                // Screen size will be set automatically
+                this.scale.setScreenSize(true);
+
+                this.scale.refresh();
+            }
+        },
+
         preload: function () {
             this.load.image('preloader', 'assets/preloader.gif');
         },
 
         create: function () {
-            // Loading screen will have a black background
-            this.game.stage.backgroundColor = '#000';
-
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
-            // Have the game centered horizontally
-            this.scale.pageAlignHorizontally = true;
-            this.scale.pageAlignVertically = true;
-
-            // Screen size will be set automatically
-            this.scale.setScreenSize(true);
-
             this.game.state.start('preloader');
         }
     };
